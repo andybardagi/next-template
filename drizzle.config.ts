3e08fd5b1898e36c9e902c1e0e1941f4afcf919e
Andrés Bardagí Inchaurrondo <71184130+andybardagi@users.noreply.github.com>
2024-03-29 17:13:55 -0300

    fix: make drizzle.config.ts use Turso

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,28 +1,13 @@
 import type { Config } from "drizzle-kit"
 
-const {
-  DATABASE_USERNAME,
-  DATABASE_PASSWORD,
-  DATABASE_HOST,
-  DATABASE_NAME,
-  DATABASE_INFRA,
-} = process.env
-
 const config = {
   schema: "./src/db/schema/*",
   out: "./src/db/migrations",
-  dbCredentials:
-    DATABASE_INFRA === "planetscale"
-      ? {
-          host: DATABASE_HOST as string,
-          user: DATABASE_USERNAME as string,
-          password: DATABASE_PASSWORD as string,
-          database: DATABASE_NAME as string,
-        }
-      : {
-          uri: `mysql://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@${DATABASE_HOST}/${DATABASE_NAME}`,
-        },
-  driver: "mysql2",
+  dbCredentials: {
+    authToken: process.env.TURSO_AUTH_TOKEN ?? "",
+    url: process.env.TURSO_DATABASE_URL ?? "",
+  },
+  driver: "turso",
 } satisfies Config
 
 export default config
